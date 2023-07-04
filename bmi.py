7346b32916cc521e071db97cab161c4529eef9a7
StefanK2ff <stefan.k2ff@googlemail.com>
2023-07-04 10:34:15 +0200

    added base functionality (input and calcluation) to program

--- bmi.py
+++ bmi.py
@@ -1,9 +1,21 @@
-# pseudo code for BMI calculator
+# pseudocode for BMI calculator
 # welcome message, display what's going to happen
 # ask for height in cm
-# ask for weight in kg's
-# calc BMI, wight / height ** 2
+# ask for weight in kgs
+# calc BMI, weight / height ** 2
 
 # ideas:
-# check if numbers make sense (add lower and upper threshholds
+# check if numbers make sense (add lower and upper thresholds
 
+globvar_welcome_message = "Welcome, this is an two-step BMI calculator. After you typed your height, it will aks your weight."
+
+def calc_bmi (weight, height):
+    return weight / height ** 2
+
+
+print(globvar_welcome_message)
+
+height = int(input("Please provide your height in cm: "))
+weight = int(input("Please provide your weight in kgs: "))
+
+print(f"Your BMI is {calc_bmi(height, weight)}")
