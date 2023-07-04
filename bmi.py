6cf89b51c455afea31797ca02e79f730a7743494
StefanK2ff <stefan.k2ff@googlemail.com>
2023-07-04 10:24:06 +0200

    added bmi.py with pseudocode for upcoming bmi calculator

---
+++ bmi.py
@@ -0,0 +1,9 @@
+# pseudo code for BMI calculator
+# welcome message, display what's going to happen
+# ask for height in cm
+# ask for weight in kg's
+# calc BMI, wight / height ** 2
+
+# ideas:
+# check if numbers make sense (add lower and upper threshholds
+
