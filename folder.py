2a387b4483dd6c6156066a968f1e7af635dd60a6
StefanK2ff <stefan.k2ff@googlemail.com>
2023-07-05 10:07:12 +0200

    playing around with rel and absolute paths

--- folder.py
+++ folder.py
@@ -3,11 +3,22 @@ import os
 print(os.listdir("."))
 # lists the files in the location the program has been started in
 
-if "testfile.txt" in os.listdir("."):
-    with open("testfile.txt", "r") as file:
+globvar_filename = "testfile.txt"
+globvar_path = 'C:/Users/stefa/Documents/Code/PycharmProjects/pythonProject2/'
+
+def line_printer(filename):
+    with open(filename, "r") as file:
         for line in file:
             print(line)
+
+if globvar_filename in os.listdir("."):
+    print("RELATIVE")
+    line_printer(globvar_filename)
+
+elif globvar_filename in os.listdir(globvar_path):
+    print("ABSOLUTE")
+    line_printer(globvar_path+globvar_filename)
+
 else:
     print("file not found!")
 
-    
