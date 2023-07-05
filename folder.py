cbaad19089794ca46de636c17930922e55648017
StefanK2ff <stefan.k2ff@googlemail.com>
2023-07-05 09:57:31 +0200

    added functionality to list content of a specific file

--- folder.py
+++ folder.py
@@ -1,4 +1,13 @@
 import os
 
-
 print(os.listdir("."))
+# lists the files in the location the program has been started in
+
+if "testfile.txt" in os.listdir("."):
+    with open("testfile.txt", "r") as file:
+        for line in file:
+            print(line)
+else:
+    print("file not found!")
+
+    
