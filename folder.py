35554073cf44be07b2d6bca38ca02f454d368206
StefanK2ff <stefan.k2ff@googlemail.com>
2023-07-05 11:25:04 +0200

    adding selection depeding on where program is exectued from

--- folder.py
+++ folder.py
@@ -1,10 +1,11 @@
 import os
 
-print(os.listdir("."))
+#print(os.listdir("."))
 # lists the files in the location the program has been started in
 
 globvar_filename = "testfile.txt"
-globvar_path = 'C:/Users/stefa/Documents/Code/PycharmProjects/pythonProject2/'
+globvar_path_replaced = os.path.abspath(os.path.join(os.path.dirname(__file__))).replace("\\","/")
+globvar_path_replaced_file = os.path.abspath(os.path.join(os.path.dirname(__file__),globvar_filename)).replace("\\","/")
 
 def line_printer(filename):
     with open(filename, "r") as file:
@@ -15,9 +16,9 @@ if globvar_filename in os.listdir("."):
     print("RELATIVE")
     line_printer(globvar_filename)
 
-elif globvar_filename in os.listdir(globvar_path):
+elif globvar_filename in os.listdir(globvar_path_replaced):
     print("ABSOLUTE")
-    line_printer(globvar_path+globvar_filename)
+    line_printer(globvar_path_replaced_file)
 
 else:
     print("file not found!")
