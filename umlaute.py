99c372b528e4afbb6d6600be868855c5fe6bf5c5
StefanK2ff <stefan.k2ff@googlemail.com>
2023-07-10 09:02:27 +0200

    added enconding parameter and a function to write a utf-8 file

--- umlaute.py
+++ umlaute.py
@@ -2,6 +2,11 @@ import os
 
 filename = os.path.join(os.path.dirname(__file__), "umlaute.txt")
 
-with open(filename, "r") as file:
+with open(filename, "r", encoding="utf-8") as file:
     for line in file:
         print(line)
+
+filename_out = os.path.join(os.path.dirname(__file__), "umlaute_output.txt")
+
+with open(filename_out, "w", encoding="utf-8") as outputfile:
+    outputfile.write("Müllerchen")
