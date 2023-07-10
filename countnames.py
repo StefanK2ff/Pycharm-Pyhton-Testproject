3a8d78cfb08356a3e86119674e10cf952418dadd
StefanK2ff <stefan.k2ff@googlemail.com>
2023-07-10 09:47:21 +0200

    added base functionality to loop over all files. TEMP, to see if it works: count lines in files

--- countnames.py
+++ countnames.py
@@ -4,13 +4,24 @@
 
 import os
 
+FILEPATH = "data/names"
+
 #try to get list of whats in the folder
 
 temp_filnamelist = os.listdir("data/names")
 
 #making sure to just list real files
 
-filenamelist = [name for name in temp_filnamelist if not os.path.isdir(os.path.join("data/names", name))]
+filenamelist = [name for name in temp_filnamelist if not os.path.isdir(os.path.join(FILEPATH, name))]
 
 
 print(filenamelist)
+
+# loop over name list , open it
+
+for filename in filenamelist:
+    with open(os.path.join(FILEPATH, filename), "r", encoding="UTF-8") as file:
+        counter = 0
+        for line in file:
+            counter += 1
+        print(filename + ": " + str(counter) + " entries")
