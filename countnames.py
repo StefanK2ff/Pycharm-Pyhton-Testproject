1fbfe868825f3fd4c1ee8fbe2c16356f96006cc5
StefanK2ff <stefan.k2ff@googlemail.com>
2023-07-10 09:39:52 +0200

    added functionality to read in all real files in the data folder

--- countnames.py
+++ countnames.py
@@ -1,3 +1,16 @@
 # program should count a specific name or - if none given - the occurences of all names.
 
 #bonus: safe result in a file
+
+import os
+
+#try to get list of whats in the folder
+
+temp_filnamelist = os.listdir("data/names")
+
+#making sure to just list real files
+
+filenamelist = [name for name in temp_filnamelist if not os.path.isdir(os.path.join("data/names", name))]
+
+
+print(filenamelist)
